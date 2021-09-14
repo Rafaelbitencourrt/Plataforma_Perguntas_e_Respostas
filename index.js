58960c31513c6dd12cf2561be02b5627c7e30db7
Rafael <rafaelbitencourrt@gmail.com>
2021-09-14 07:12:53 -0300

    ok

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bem vindo ao meu site");
+});
+
+app.listen(PORT, () => {
+  console.log("app rodando");
+});
