a21413b93cb0825bc6814aeb5c97f6d324d6bdc1
Rafael <rafaelbitencourrt@gmail.com>
2021-09-17 15:29:35 -0300

    OK

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 
 //DATABASE
 
@@ -68,6 +69,18 @@ app.get("/pergunta/:id", (req, res) => {
       //pergunta não encontrada
       res.redirect("/");
     }
+  });
+});
+
+app.post("/responder", (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId,
+  }).then(() => {
+    res.redirect("/pergunta/" + perguntaId);
   });
 });
 
